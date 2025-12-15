1739a478a48f5c6c59dab2bb5c0c3793decf3c62
Akash <akashmishra@005a>
2025-12-15 19:11:14 +0530

    Add toss feature

--- src/SplashScreen/FrontPage.java
+++ src/SplashScreen/FrontPage.java
@@ -1,21 +1,46 @@
 package SplashScreen;
 import java. util.Scanner;
-public class FrontPage {
-    public static void teamName()
+class CricketTeam
+{
+    String tname1;
+    String tname2;
+
+    public void tossWinnerName()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the name of toss winner name ");
+        String str = sc.nextLine();
+        if(str.equalsIgnoreCase(tname1)||str.equalsIgnoreCase(tname2)){
+            System.out.println("congartulation " +str + " win ");
+            tossWinnerName();
+        }
+          else {
+            System.out.println("something went wrong ");
+            tossWinnerName();
+
+        }
+
+    }
+
+    public  void teamName()
     {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your first team name ");
-        String tname1 = sc.nextLine();
-        System.out.println("Enter your first team name ");
-        String tname2 = sc.nextLine();
-        System.out.println("Secoand team is "+tname1);
+        tname1 = sc.nextLine();
+        System.out.println("Enter your secoand team name ");
+        tname2 = sc.nextLine();
+        System.out.println("first team is "+tname1);
         System.out.println("Secaond  team is "+tname2);
     }
+}
+public class FrontPage {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("welcome to Scoring area");
         System.out.println("enter your team name  ");
-        teamName();
+        CricketTeam ct = new CricketTeam();
+        ct.teamName();
+      ct.tossWinnerName();
 
     }
 }
